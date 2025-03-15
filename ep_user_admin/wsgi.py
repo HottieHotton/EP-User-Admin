928373607e07e9c293f4ef473aa331cf0de2fb75
Braxton Hotton <bhotton25@gmail.com>
2025-03-15 17:28:28 -0600

    Changing variable name

--- ep_user_admin/wsgi.py
+++ ep_user_admin/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ep_user_admin.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
